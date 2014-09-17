6de6959065b94b96f01300dd8fced26cf73d7aa9
dhiwakaran <dhiwakaran.murugesan@sibylgaze.com>
2014-09-17 11:48:27 +0530

    FirstCode

---
+++ IdeaProjects/workshop/src/main/java/com/canviara/chartView.java
@@ -0,0 +1,38 @@
+package com.canviara;
+
+import javax.annotation.PostConstruct;
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import org.primefaces.model.chart.PieChartModel;
+
+@ManagedBean
+public class chartView implements Serializable {
+
+    private PieChartModel pieModel1;
+
+
+    @PostConstruct
+    public void init() {
+        chartData();
+    }
+
+    public PieChartModel getpieModel1() {
+        return pieModel1;
+    }
+
+
+
+    private void chartData() {
+        pieModel1 = new PieChartModel();
+
+        pieModel1.set("2011-12", 540);
+        pieModel1.set("2012-13", 325);
+        pieModel1.set("2013-14", 702);
+        pieModel1.set("2014-15", 421);
+
+        pieModel1.setTitle("Sales Volume");
+        pieModel1.setLegendPosition("w");
+    }
+
+
+}
